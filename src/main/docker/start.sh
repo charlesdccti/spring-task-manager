4b3a94cc8b0f23730afc5afbdeecbf488d8063d1
tsarenkotxt <tsarenkotxt@gmail.com>
2017-06-21 05:01:23 +0300

    docker-compose 2.1 healthcheck

--- src/main/docker/start.sh
+++
@@ -1,8 +0,0 @@
-#!/bin/bash
-
-while ! exec 6<>/dev/tcp/${DATABASE_HOST}/${DATABASE_PORT}; do
-    echo "Trying to connect to MySQL at ${DATABASE_PORT}..."
-    sleep 10
-done
-
-/usr/local/tomcat/bin/catalina.sh run
